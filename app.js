b75380385017bc2914b14766581826297f5173fa
Alex Minge <aminge37@gmail.com>
2016-08-09 21:22:33 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+app.use('/', index);
+
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), function() {
+  console.log('Listening on port: ', app.get('port'));
+});
---
+++ public/scripts/factories/dataFactory.js
@@ -0,0 +1,14 @@
+myApp.factory('DataFactory', ['$http', function($http) {
+
+  // PRIVATE
+
+
+
+  // PUBLIC
+
+  var publicAPI = {
+
+  };
+
+  return publicAPI;
+}]);
