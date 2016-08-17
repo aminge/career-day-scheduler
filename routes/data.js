11984cb640a0043d1f63d2e0a6eb8c4e0f16ccad
Alex Minge <aminge37@gmail.com>
2016-08-17 17:28:07 -0500

    Starting to add routes in data.js

--- routes/data.js
+++ routes/data.js
@@ -2,3 +2,24 @@ var express = require('express');
 var router = express.Router();
 var pg = require('pg');
 var connectionString = require('./connect');
+
+router.get('/recruiters', function(req, res) {
+
+});
+
+router.get('/students', function(req, res) {
+
+});
+
+router.put('/recruiter', function(req, res) {
+
+});
+
+router.put('/schedule', function(req, res) {
+
+});
+
+router.put('/student', function(req, res) {
+
+});
+
