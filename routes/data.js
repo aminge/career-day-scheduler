53541c0966fe90ac27eebe51ff285cfb8fdaee1c
Alex Minge <aminge37@gmail.com>
2016-08-21 20:59:25 -0500

    Adding database connections and SQL statements for inserting data

--- routes/data.js
+++ routes/data.js
@@ -16,7 +16,7 @@ router.get('/students', function(req, res) {
 });
 
 router.get('/students/:cohortID', function(req, res) {
-  var cohortID = req.cohortID;
+  var cohortID = req.params.cohortID;
 });
 
 router.put('/event', function(req, res) {
@@ -25,6 +25,20 @@ router.put('/event', function(req, res) {
     startTime: req.startTime
     // I need to make sure this is getting passed in on the front end
   };
+
+  pg.connect(connectionString, function(err, client, done) {
+    client.query('INSERT INTO events (name, startTime) VALUES ($1, $2)',
+      [event.name, event.starTime],
+      function (err, result) {
+        if (err) {
+          console.log("Error inserting data: ", err);
+          res.send(false);
+        } else {
+          res.send(result);
+        }
+      }
+    );
+  });
 });
 
 router.put('/recruiter', function(req, res) {
@@ -33,6 +47,20 @@ router.put('/recruiter', function(req, res) {
     email: req.email,
     company: req.company
   };
+
+  pg.connect(connectionString, function(err, client, done) {
+    client.query('INSERT INTO recruiters (name, email, company) VALUES ($1, $2, $3)',
+      [recruiter.name, recruiter.email, recruiter.company],
+      function (err, result) {
+        if (err) {
+          console.log("Error inserting data: ", err);
+          res.send(false);
+        } else {
+          res.send(result);
+        }
+      }
+    );
+  });
 });
 
 router.put('/schedule', function(req, res) {
@@ -43,7 +71,21 @@ router.put('/student', function(req, res) {
   var student = {
     name: req.name,
     email: req.email,
-    company: req.company
-  }
+    cohort: req.cohort
+  };
+
+  pg.connect(connectionString, function(err, client, done) {
+    client.query('INSERT INTO students (name, email, cohort) VALUES ($1, $2, $3)',
+      [student.name, student.email, student.cohort],
+      function (err, result) {
+        if (err) {
+          console.log("Error inserting data: ", err);
+          res.send(false);
+        } else {
+          res.send(result);
+        }
+      }
+    );
+  });
 });
 
