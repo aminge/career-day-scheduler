eb6cc815cf943ec02972ba0293d5886ed9aa5211
Alex Minge <aminge37@gmail.com>
2016-09-13 20:34:24 -0500

    Added more test data

--- sampleData.js
+++ sampleData.js
@@ -1,23 +1,47 @@
-var sampleCohort = {
-  name: 'Iota',
-  students: [
-    {name: 'Alex Minge', cohort: 'Iota', email: 'alex@gmail.com'},
-    {name: 'Caitlin Collins', cohort: 'Iota', email: 'caitlin@gmail.com'},
-    {name: 'Kat Traxler', cohort: 'Iota', email: 'kat@gmail.com'},
-    {name: 'Savio Nguyen', cohort: 'Iota', email: 'savio@gmail.com'},
-    {name: 'Anthony Macki', cohort: 'Iota', email: 'anthony@gmail.com'},
-    {name: 'Annette Tousley', cohort: 'Iota', email: 'annette@gmail.com'},
-    {name: 'Moni Francesca', cohort: 'Iota', email: 'moni@gmail.com'},
-    {name: 'Remy Allen', cohort: 'Iota', email: 'remy@gmail.com'},
-    {name: 'Andrew Glenn', cohort: 'Iota', email: 'andrew@gmail.com'},
-    {name: 'Andy Fehrenbach', cohort: 'Iota', email: 'andy@gmail.com'},
-    {name: 'Josh Sternberg', cohort: 'Iota', email: 'josh@gmail.com'},
-    {name: 'Juan Ball', cohort: 'Iota', email: 'juan@gmail.com'},
-    {name: 'John Rauchy', cohort: 'Iota', email: 'john@gmail.com'},
-    {name: 'Gwen Williams', cohort: 'Iota', email: 'gwen@gmail.com'},
-    {name: 'Luis de la Espriella', cohort: 'Iota', email: 'luis@gmail.com'},
-    {name: 'Kailey Colestock', cohort: 'Iota', email: 'kailey@gmail.com'},
-    {name: 'Bennett Batzli', cohort: 'Iota', email: 'bennett@gmail.com'},
-    {name: 'Kati Lueth', cohort: 'Iota', email: 'kati@gmail.com'}
-  ]
-}
+var students = [
+  {
+    name: 'Iota',
+    students: [
+      {name: 'Alex Minge', cohort: 'Iota', email: 'alex@gmail.com'},
+      {name: 'Caitlin Collins', cohort: 'Iota', email: 'caitlin@gmail.com'},
+      {name: 'Kat Traxler', cohort: 'Iota', email: 'kat@gmail.com'},
+      {name: 'Savio Nguyen', cohort: 'Iota', email: 'savio@gmail.com'},
+      {name: 'Anthony Macki', cohort: 'Iota', email: 'anthony@gmail.com'},
+      {name: 'Annette Tousley', cohort: 'Iota', email: 'annette@gmail.com'},
+      {name: 'Moni Francesca', cohort: 'Iota', email: 'moni@gmail.com'},
+      {name: 'Remy Allen', cohort: 'Iota', email: 'remy@gmail.com'},
+      {name: 'Andrew Glenn', cohort: 'Iota', email: 'andrew@gmail.com'},
+      {name: 'Andy Fehrenbach', cohort: 'Iota', email: 'andy@gmail.com'},
+      {name: 'Josh Sternberg', cohort: 'Iota', email: 'josh@gmail.com'},
+      {name: 'Juan Ball', cohort: 'Iota', email: 'juan@gmail.com'},
+      {name: 'John Rauchy', cohort: 'Iota', email: 'john@gmail.com'},
+      {name: 'Gwen Williams', cohort: 'Iota', email: 'gwen@gmail.com'},
+      {name: 'Luis de la Espriella', cohort: 'Iota', email: 'luis@gmail.com'},
+      {name: 'Kailey Colestock', cohort: 'Iota', email: 'kailey@gmail.com'},
+      {name: 'Bennett Batzli', cohort: 'Iota', email: 'bennett@gmail.com'},
+      {name: 'Kati Lueth', cohort: 'Iota', email: 'kati@gmail.com'}]
+  },
+  {
+    name: 'Kappa',
+    students: [
+      {name: 'Enrique Ortega', cohort: 'Kappa', email: 'enrique@gmail.com'},
+      {name: 'Matthew Keymer', cohort: 'Kappa', email: 'matthew@gmail.com'},
+      {name: 'Tracey Van Haaften', cohort: 'Kappa', email: 'tracey@gmail.com'},
+      {name: 'Aaron McGrath', cohort: 'Kappa', email: 'aaron@gmail.com'},
+      {name: 'Biz Cook', cohort: 'Kappa', email: 'biz@gmail.com'},
+      {name: 'Brady Peterson', cohort: 'Kappa', email: 'brady@gmail.com'},
+      {name: 'Carl Peaslee', cohort: 'Kappa', email: 'carl@gmail.com'},
+      {name: 'Evan Kimlinger', cohort: 'Kappa', email: 'evan@gmail.com'},
+      {name: 'Hank Andre', cohort: 'Kappa', email: 'hank@gmail.com'},
+      {name: 'Jeremy Hjelmeland', cohort: 'Kappa', email: 'jeremy@gmail.com'},
+      {name: 'Joette Poehler', cohort: 'Kappa', email: 'joette@gmail.com'},
+      {name: 'Kenzie Bultema', cohort: 'Kappa', email: 'kenzie@gmail.com'},
+      {name: 'Mark Rothermal', cohort: 'Kappa', email: 'mark@gmail.com'},
+      {name: 'Miles Johnson', cohort: 'Kappa', email: 'miles@gmail.com'},
+      {name: 'Mike Elliott', cohort: 'Kappa', email: 'mike@gmail.com'},
+      {name: 'Roman Hiebert', cohort: 'Kappa', email: 'roman@gmail.com'},
+      {name: 'Riley Harrison', cohort: 'Kappa', email: 'riley@gmail.com'},
+      {name: 'Michelle Wang', cohort: 'Kappa', email: 'michelle@gmail.com'},
+      {name: 'Tommy Chester', cohort: 'Kappa', email: 'Tommy Chester@gmail.com'}]
+  },
+];
